fea21b650e7d124521d5a4dbd7cc14f3ac3e38c1
FSopelsa <felix.sopelsa@gmail.com>
2026-06-24 10:24:42 +0200

    first commit

---
+++ src/main/java/se/lexicon/Contact.java
@@ -0,0 +1,17 @@
+package se.lexicon;
+
+public class Contact {
+    private int id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    public Contact(int id, String name, String email, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // getters, setters, toString()
+}
---
+++ src/main/java/se/lexicon/ContactApp.java
@@ -0,0 +1,9 @@
+package se.lexicon;
+
+public class ContactApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Contact App!");
+
+        // interact with the user and manage contacts
+    }
+}
